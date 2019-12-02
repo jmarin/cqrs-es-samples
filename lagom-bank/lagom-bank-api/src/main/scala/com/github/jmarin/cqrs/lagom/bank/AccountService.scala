e3a25a93d0e343766fe9a2c95b83b32a8111c952
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-12-02 09:31:17 -0500

    Add service definition

---
+++ lagom-bank/lagom-bank-api/src/main/scala/com/github/jmarin/cqrs/lagom/bank/AccountService.scala
@@ -0,0 +1,29 @@
+package com.github.jmarin.cqrs.lagom.bank
+
+import akka.NotUsed
+import com.lightbend.lagom.scaladsl.api.Service
+import com.lightbend.lagom.scaladsl.api.Descriptor
+import com.lightbend.lagom.scaladsl.api.transport.Method
+import com.lightbend.lagom.scaladsl.api.ServiceCall
+
+trait AccountService extends Service {
+
+  def createAccount: ServiceCall[CreateAccount, Account]
+  def deposit(id: String): ServiceCall[DepositMoney, Account]
+  def withdraw(id: String): ServiceCall[WithdrawMoney, Account]
+  def get(id: String): ServiceCall[NotUsed, Account]
+  def transfer(id: String): ServiceCall[TransferToAccount, Account]
+
+  override def descriptor: Descriptor = {
+    import Service._
+    named("accounts")
+      .withCalls(
+        restCall(Method.POST, "/accounts", createAccount),
+        restCall(Method.PUT, "/accounts/:id/deposit", deposit _),
+        restCall(Method.PUT, "/accounts/:id/withdraw", withdraw _),
+        restCall(Method.PUT, "/accounts/:id/transer", transfer _),
+        restCall(Method.GET, "/accounts/:id", get _)
+      )
+      .withAutoAcl(true)
+  }
+}
