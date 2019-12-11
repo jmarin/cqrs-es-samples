5640691cb4b168414a732c94a61d79152e60ebb1
Juan Marin Otero <juan.m.otero@pwc.com>
2019-12-11 16:11:23 -0500

    Add Events

---
+++ lagom-bank/lagom-bank-impl/src/main/scala/com/github/jmarin/cqrs/lagom/bank/AccountEvents.scala
@@ -0,0 +1,36 @@
+package com.github.jmarin.cqrs.lagom.bank
+
+import play.api.libs.json.OFormat
+import play.api.libs.json.Json
+
+sealed trait AccountEvent 
+
+case class AccountOpened(account: Account) extends AccountEvent
+
+object AccountOpened {
+    implicit val format: OFormat[AccountOpened] = Json.format
+}
+
+case class Deposited(amount: BigDecimal) extends AccountEvent
+
+object Deposited {
+    implicit val format: OFormat[Deposited] = Json.format
+}
+
+case class Withdrawn(amount: BigDecimal) extends AccountEvent\
+
+object Deposited {
+    implicit val format: OFormat[Deposited] = Json.format
+}
+
+case class MoneyTransferred(to: String, amount: BigDecimal) extends AccountEvent
+
+object MoneyTransferred {
+    implicit val format: OFormat[MoneyTransferred] = Json.format
+}
+
+case class TransferFeeDeducted(amount: BigDecimal) extends AccountEvent
+
+object TransferFeeDeducted {
+    implicit val format: OFormat[TransferFeeDeducted] = Json.format
+}
