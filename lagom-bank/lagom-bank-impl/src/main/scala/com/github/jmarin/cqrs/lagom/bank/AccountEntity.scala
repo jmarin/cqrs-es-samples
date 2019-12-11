b3aee62d0f75850310b9a3c7a2740ec4f375d289
Juan Marin Otero <juan.m.otero@pwc.com>
2019-12-11 17:13:05 -0500

    Add Entity with test

---
+++ lagom-bank/lagom-bank-impl/src/main/scala/com/github/jmarin/cqrs/lagom/bank/AccountEntity.scala
@@ -0,0 +1,69 @@
+package com.github.jmarin.cqrs.lagom.bank
+
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity
+import com.typesafe.config.ConfigFactory
+
+class AccountEntity extends PersistentEntity {
+
+    override type Command = AccountCommand[_]
+    override type Event = AccountEvent
+    override type State = AccountState
+
+    override def initialState: State = EmptyState()
+
+    val config = ConfigFactory.load()
+    val transferFee = config.getDouble("bank.transfer-fee")
+
+    override def behavior: Behavior = {
+        case AccountState(_, _) =>
+         Actions()
+         .onReadOnlyCommand[Get.type, AccountState] {
+             case (Get, ctx, state) =>
+               ctx.reply(state)
+         }
+         .onCommand[OpenAccount, OpenAccountDone] {
+             case (OpenAccount(account), ctx, state) if !state.open =>
+               ctx.thenPersist(AccountOpened(account)) { _ =>
+                 ctx.reply(OpenAccountDone(account))
+               }
+         }
+         .onCommand[Deposit, DepositDone] {
+             case (Deposit(amount), ctx, state) if amount > 0 =>
+              ctx.thenPersist(Deposited(amount)) { _ =>
+                ctx.reply(DepositDone(amount))
+              }
+         }
+         .onCommand[Withdraw, WithdrawDone] {
+             case (Withdraw(amount), ctx, state) =>
+                 if (amount < state.balance) {
+                     ctx.thenPersist(Withdrawn(amount)) { _ =>
+                       ctx.reply(WithdrawDone(amount))
+                    }
+                 } else {
+                     ctx.commandFailed(AccountException("Insufficient Funds"))
+                     ctx.done
+                 }
+         }
+         .onCommand[TransferMoney, TransferMoneyDone] {
+             case (TransferMoney(to, amount), ctx, state) =>
+               if ((amount + transferFee) < state.balance) {
+                   val evts = Seq(
+                     MoneyTransferred(to, amount),
+                     TransferFeeDeducted(transferFee)
+                   )
+                   ctx.thenPersistAll(evts: _*){ () =>
+                     ctx.reply(TransferMoneyDone(to, amount))
+                   }
+               } else {
+                   ctx.commandFailed(AccountException("Insufficient Funds"))
+                   ctx.done
+               }
+         }
+         .onEvent(eventHandler)
+    }
+
+    def eventHandler: EventHandler = {
+        case (e: AccountEvent, state) => state.applyEvent(e)
+    }
+
+}
