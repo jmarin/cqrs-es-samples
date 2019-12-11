3625d3931acf30a5c66d5f3e04b3abefd259c9a2
Juan Marin Otero <juan.m.otero@pwc.com>
2019-12-11 13:46:39 -0500

    remove api

--- lagom-bank/lagom-bank-api/src/main/scala/com/github/jmarin/cqrs/lagom/bank/Account.scala
+++
@@ -1,9 +0,0 @@
-package com.github.jmarin.cqrs.lagom.bank
-
-import play.api.libs.json.{Format, Json}
-
-case class Account(id: String, balance: BigDecimal)
-
-object Account {
-  implicit val format: Format[Account] = Json.format
-}
